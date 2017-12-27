89182b2f31e2a4931eec65a0c23bfd4ae4c2abba
DEVZONEYDS\USER <kwanwoo5@naver.com>
2017-12-27 14:45:35 +0900

    first commit

---
+++ LinkedListStudy/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> list = new LinkedList<string>();
+            list.AddLast("Apple");
+            list.AddLast("Banana");
+            list.AddLast("Lemon");
+
+            LinkedListNode<string> node = list.Find("Banana");
+            LinkedListNode<string> newNode = new LinkedListNode<string>("Grape");
+
+            // 새 Grape 노드를 Banana 노드 뒤에 추가
+            list.AddAfter(node, newNode);
+
+            // 리스트 출력
+            list.ToList().ForEach(p => Console.WriteLine(p));
+
+            // Enumerator를 이용한 리스트 출력
+            //foreach (var m in list)
+            //{
+            //    Console.WriteLine(m);
+            //}
+        }
+    }
+}
